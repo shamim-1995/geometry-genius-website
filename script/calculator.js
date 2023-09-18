7397e4e010690c631bbbcc77b233d3ffc3cc2e50
shamim-1995 <sashamim987@gmail.com>
2023-09-18 23:17:24 +0600

    added update section

--- script/calculator.js
+++ script/calculator.js
@@ -23,6 +23,7 @@ function calculateTriangleArea() {
 
     const areaSpan = document.getElementById('triangle area');
     areaSpan.innerText = area;
+    addToCalculationEntry('Triangle', area);
 }
 
 function calculateRectangleArea() {
@@ -40,6 +41,14 @@ function calculateRectangleArea() {
     const length = parseFloat(lengthValueText);
     console.log(length);
 
+
+    // Validate input:width and length
+
+    if(isNaN(width) || isNaN(length)){
+        alert('Please input a number.');
+        return;
+    }
+
     // Calculate area.
     const area = width * length;
     console.log(area);
@@ -48,6 +57,8 @@ function calculateRectangleArea() {
 
     const areaSpan = document.getElementById('rectangle area');
     areaSpan.innerText = area;
+
+    addToCalculationEntry('Rectangle', area);
 }
 
 
@@ -55,13 +66,23 @@ function calculateRectangleArea() {
 
 function calculateParallelogramArea() {
     const base = getInputValue('parallelogram base');
-    console.log(base);
+    // console.log(base);
 
     const height = getInputValue('parallelogram height');
     console.log(height);
 
+    // Validate input.
+    if(isNaN(base) || isNaN(height)){
+        alert('Please insert a number.');
+        return;
+    }
+
     const area = base * height;
      setElementInnerText('parallelogram area', area);
+
+     // Add to calculation entry.
+
+     addToCalculationEntry('parallelogram', area)
 
 
 }
@@ -73,7 +94,10 @@ function calculateParallelogramArea() {
     const majorRadius = getInputValue('ellipse major radius');
     const minorRadius = getInputValue('ellipse minor radius');
     const area = 3.14 * majorRadius * minorRadius;
-    setElementInnerText('ellipse area', area);
+    const areaTwoDecimal = area.toFixed(2);
+    setElementInnerText('ellipse area', areaTwoDecimal);
+
+    addToCalculationEntry('Ellipse', areaTwoDecimal);
  }
 
 
@@ -98,3 +122,47 @@ function setElementInnerText(elementId, area){
 
 
 }
+
+
+// Add to calculation entry.
+
+/*
+
+1. get the element where you want to add the dynamic HTML.
+
+2.create an element you want to add.
+
+3. If needed add some class.
+
+4. set innerHTML . it could be dynamic Template string.
+
+5. append the created element as a child of the parent.
+*/ 
+
+function addToCalculationEntry(areaType, area){
+    console.log(areaType+ ' ' + area);
+    const calculationEntry = document.getElementById('calculation-entry');
+
+    const count = calculationEntry.childElementCount;
+
+    const p = document.createElement('p');
+    p.classList.add('my-4');
+    p.innerHTML = `${count + 1}. ${areaType} ${area} cm<sup>2</sup> <button class="btn btn-sm btn-success">Convert</button>`;
+
+    calculationEntry.appendChild(p);
+
+}
+
+
+
+
+// Data validation.
+
+/*
+1. set the proper type of the input field.(number, data, email).
+
+2. check type using typeof.
+
+3. ssNaN:means Not a Number. isNaN is checking whether the input is not a number or not.
+
+*/ 
